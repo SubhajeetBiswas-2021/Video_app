1325788cf5096184d001da2ef6a99cca1cb9fc47
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-09-12 22:46:09 +0530

    commit

---
+++ app/src/main/java/com/subhajeet/videoapp/Data/models/VideoModels.kt
@@ -0,0 +1,12 @@
+package com.subhajeet.videoapp.Data.models
+
+
+data class VideoModels (
+    var id:String?,
+    var path:String?,
+    var title:String?,
+    var fileName : String?,
+    var size : String?,
+    var duration: String?,
+    var dateAdded : String?
+)
