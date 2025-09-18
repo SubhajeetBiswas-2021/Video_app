78f10444ade24eca57e955e5ed836ea4b14241d8
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-09-18 05:31:52 +0530

    commit

---
+++ app/src/main/java/com/subhajeet/videoapp/Presentation/MyViewModel.kt
@@ -0,0 +1,31 @@
+package com.subhajeet.videoapp.Presentation
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.subhajeet.videoapp.Data.models.VideoModels
+import com.subhajeet.videoapp.Domain.Repo.useCase.GetAllVideosUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyViewModel @Inject constructor(private val getAllVideosUseCase: GetAllVideosUseCase , val application: Application) : ViewModel() {
+
+
+    init {
+        loadAllVideos()
+    }
+    val videosList = MutableStateFlow<List<VideoModels>>(emptyList())
+    fun loadAllVideos(){
+        viewModelScope.launch {
+            getAllVideosUseCase.getAllVideosUserCase(application)
+                .collectLatest {
+                    videosList.value = it
+                }
+        }
+    }
+
+}
